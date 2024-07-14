5b35931755273178576c611b9f15028254e3c040
ygrfil <ygrfil@gmail.com>
2024-07-15 00:53:34 +0800

    Removed the `ALLOWED_USER_IDS` configuration from the environment.

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ ENV = {
     "TELEGRAM_BOT_TOKEN": os.getenv("TELEGRAM_BOT_TOKEN"),
     "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
     "ANTHROPIC_API_KEY": os.getenv("ANTHROPIC_API_KEY"),
-    "ALLOWED_USER_IDS": os.getenv("ALLOWED_USER_IDS", "").split(","),
     "ADMIN_USER_IDS": os.getenv("ADMIN_USER_IDS", "").split(","),
 }
 
